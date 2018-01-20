c657c4431e505133c9f976c8b5f0a32dfae82607
Chad Johnson <chadj@users.noreply.github.com>
2018-01-19 20:36:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# ANT-WS
+
+ANT-WS is a simple server written in javascript for Node.js that when combined with an ANT+ usb stick can relay all ant+ messages in range over a websocket to browser based clients.  ANT-WS relies upon the [ant-plus](https://github.com/Loghorn/ant-plus) Node.js package for handling low level serial communication with the ant+ usb stick.
+
+## Prerequisites
+
+The [usb](https://github.com/tessel/node-usb) module is included as a dependency. On Linux, you'll need libudev to build the [usb](https://github.com/tessel/node-usb) module. On Ubuntu/Debian: `sudo apt-get install build-essential libudev-dev`.  On Windows please follow the [instructions](https://github.com/chadj/ant-ws/wiki/Windows-Prerequisites) in the wiki.  No special steps are required on Mac OS.
+
+## Installation
+
+1. Install [Node.js](https://nodejs.org/)
+1. Download and unpack the [ANT-WS](https://github.com/chadj/ant-ws/archive/master.zip) zip file
+1. Open a command prompt and switch directory to the unzipped folder
+1. Run the command `npm install`
+1. Start the server with `node antws.js`
+
+## Command line help
+
+```
+$ node antws.js --help
+Usage: node antws.js --port 8000
+
+Options:
+  -h, --help           Command line usage
+  -p, --port           Port to listen on
+  -v, --verbose        Verbose ant+ message logging
+```
+
+## Client Usage
+
+ANT-WS uses [Faye](https://faye.jcoglan.com/) as the underlying transport to relay ANT+ messages to browser based clients.
+
+Include the client script from the ANT-WS server at:  http://localhost:8000/client.js
+
+```html
+<script type="text/javascript" src="http://localhost:8000/client.js"></script>
+```
+
+Subscribe to and receive ant messages
+
+```javascript
+var client = new Faye.Client('http://localhost:8000/');
+client.subscribe('/bike_power', msg => {
+    const ant = JSON.parse(msg.text);
+}
+```
+
+The following topics are exposed and correspond to an ANT+ device type:
+
+* hr
+* bike_power
+* speed_cadence
+* fitness_equipment
+* stride_speed_distance
+
+An example ANT+ messages:
+```json
+{
+  "offset":0,
+  "DeviceID":1,
+  "Cadence":165,
+  "AccumulatedPower":50374,
+  "Power":283,
+  "type":"bike_power",
+  "eventName":"powerData"
+}
+```
---
+++ antws.js
@@ -0,0 +1,132 @@
+const Ant = require('ant-plus');
+const http = require('http');
+const faye = require('faye');
+const ip = require('ip');
+const parseArgs = require('minimist');
+
+const sensors = {
+  'hr': {
+    klass: Ant.HeartRateScanner,
+    instances: [],
+    dataEventNames: ['hbData'],
+  },
+  'bike_power': {
+    klass: Ant.BicyclePowerScanner,
+    instances: [],
+    dataEventNames: ['powerData'],
+  },
+  'speed_cadence': {
+    klass: Ant.SpeedCadenceScanner,
+    instances: [],
+    dataEventNames: ['cadenceData', 'speedData'],
+  },
+  'fitness_equipment': {
+    klass: Ant.FitnessEquipmentScanner,
+    instances: [],
+    dataEventNames: ['fitnessData'],
+  },
+  'stride_speed_distance': {
+    klass: Ant.StrideSpeedDistanceScanner,
+    instances: [],
+    dataEventNames: ['ssdData'],
+  }
+};
+
+class SensorManager {
+  constructor (sensor, stickManager) {
+    this.sensor = sensor;
+    this.stickManager = stickManager;
+  }
+
+  scan() {
+    return new Promise((resolve,reject) => {
+      let executed = false;
+      let listener = () => {
+        this.sensor.removeListener('attached', listener);
+        if(!executed) {
+          executed = true;
+          resolve();
+        }
+      }
+
+      this.sensor.on('attached', listener);
+      this.sensor.scan();
+    });
+  }
+}
+
+class StickManager {
+  constructor (stick) {
+    this.stick = stick;
+  }
+
+  startup() {
+    return new Promise((resolve,reject) => {
+      let executed = false;
+      let listener = () => {
+        this.stick.removeListener('startup', listener);
+        if(!executed) {
+          executed = true;
+          resolve();
+        }
+      }
+
+      this.stick.on('startup', listener);
+      if (!this.stick.open()) {
+        reject(new Error('Stick not found!'))
+      }
+    });
+  }
+}
+
+const argv = parseArgs(process.argv.slice(2), {boolean: ['help','verbose'], alias: {p: 'port', h: 'help', v: 'verbose'}, default: {port: 8000}});
+
+if(argv.help) {
+  console.log("Usage: node antws.js --port 8000\n\nOptions:\n  -h, --help           Command line usage\n  -p, --port           Port to listen on\n  -v, --verbose        Verbose ant+ message logging");
+} else {
+  const server = http.createServer();
+  const bayeux = new faye.NodeAdapter({mount: '/', timeout: 120, ping: 30});
+  bayeux.attach(server);
+  server.listen(parseInt(argv.port));
+
+  console.log("ANT-WS Server URL: http://" + ip.address() + ":" + parseInt(argv.port) + "/ or http://localhost:" + parseInt(argv.port) + "/");
+  console.log("CTRL-C to exit\n");
+
+  const stickManagers = [new StickManager(new Ant.GarminStick3), new StickManager(new Ant.GarminStick2)];
+
+  (async function() {
+    let client = bayeux.getClient();
+
+    let stickFound = false;
+    for(const stickManager of stickManagers) {
+      if(stickManager.stick.is_present()) {
+        console.log("Found ant+ usb stick");
+        await stickManager.startup();
+        stickFound = true;
+        console.log("Ant+ usb stick initialized.");
+        for(const [name, {klass, instances, dataEventNames}] of Object.entries(sensors)) {
+          const sensorManager = new SensorManager(new klass(stickManager.stick), stickManager);
+          await sensorManager.scan();
+          instances.push(sensorManager);
+          for(const dataEventName of dataEventNames) {
+            sensorManager.sensor.on(dataEventName, data => {
+              data.type = name;
+              data.eventName = dataEventName;
+              let publication = client.publish('/'+name, {text: JSON.stringify(data)});
+              if(argv.verbose) {
+                console.log(Object.entries(data).map(d => d[0] + ": " + d[1]).join(", "));
+              }
+              console.log(JSON.stringify(data));
+            });
+          }
+        }
+        console.log("Listening for ant+ messages.");
+      }
+    }
+    if(!stickFound) {
+      console.log('No Ant+ usb stick found.  ');
+    }
+  })().catch(error => {
+    console.log("Error: ", error);
+  });
+}
